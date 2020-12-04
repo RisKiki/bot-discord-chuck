75a27c5e75648658cea20dd47790b2845207da5a
Alexandre Disdier <alexandre.disdier@eisti.fr>
2020-12-04 16:09:30 +0100

    [ JokeCount ] - Bug solved

--- commands/jokeCount.js
+++ commands/jokeCount.js
@@ -12,6 +12,20 @@ class JokeCount {
 
     getCount() {
         return axios.get("http://api.icndb.com/jokes/count")
+        .then(
+            res => {
+                if (res.data.type === 'success') {
+                    this.message.channel.send('Chuko Norris possède ' + res.data.value.toString() + ' jokes !')
+                } else {
+                    this.message.channel.send('Chuck Norris ne lit pas le moldu, ré-écrit, en mieux.');
+                }
+            }
+        ).catch(
+            err => {
+                console.log(err);
+                this.message.channel.send('Chuck Norris à casser ses blagues, veuillez ressayer plus tard.');
+            }
+        )
     }
 }
 
